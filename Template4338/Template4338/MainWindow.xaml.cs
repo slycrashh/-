39f81c5b465828397a7c08979810b16b40622b86
Ravil <registermob9@gmail.com>
2024-03-21 08:09:10 +0300

    fdsfsdfdsfrsdgdsghdfgrtdfg

--- Template4338/Template4338/MainWindow.xaml.cs
+++ Template4338/Template4338/MainWindow.xaml.cs
@@ -16,13 +16,18 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using Excel = Microsoft.Office.Interop.Excel;
+using Word = Microsoft.Office.Interop.Word;
+using System.IO;
+using Newtonsoft.Json;
+using static Template4338.MainWindow;
+using Microsoft.Office.Interop.Excel;
 
 namespace Template4338
 {
     /// <summary>
     /// Логика взаимодействия для MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : System.Windows.Window
     {
         public MainWindow()
         {
@@ -62,7 +67,7 @@ namespace Template4338
                 entities.SaveChanges();
             }
         }
-
+        person[] p;
 
         private void b3_Click(object sender, RoutedEventArgs e)
         {
@@ -89,6 +94,7 @@ namespace Template4338
                     s.vozrast = ageInYears;
                 }
             }
+
             var groupedPeople = allStudents.GroupBy(p => GetCategory(p.vozrast)).ToList();
             var cat1 = allStudents.Where(p => GetCategory(p.vozrast) == 1);
             var cat2 = allStudents.Where(p => GetCategory(p.vozrast) == 2);
@@ -129,6 +135,121 @@ namespace Template4338
                 return 3;
             else
                 return 5;
+        }
+        public class person
+        {
+            public int Id { get; set; }
+            public string FullName { get; set; }
+            public string CodeClient { get; set; }
+            public string BirthDate { get; set; }
+            public string Index { get; set; }
+            public string City { get; set; }
+            public string Street { get; set; }
+            public string Home { get; set; }
+            public string Kvartira { get; set; }
+            public string E_mail { get; set; }
+        }
+
+        private void b5_Click(object sender, RoutedEventArgs e)
+        {
+            List<isrpo> isrpos;
+            using (sadEntities entities = new sadEntities())
+            {
+                isrpos =
+                entities.isrpo.ToList().OrderBy(s =>
+                s.id).ToList();
+            }
+            foreach (isrpo s in isrpos)
+            {
+                if (s.DAtta != "")
+                {
+                    //MessageBox.Show(s.DAtta);
+                    DateTime birthDate = DateTime.ParseExact(s.DAtta, "dd.MM.yyyy", null);
+                    TimeSpan age = DateTime.Today - birthDate;
+                    int ageInYears = (int)age.TotalDays / 365;
+                    s.vozrast = ageInYears;
+                }
+            }
+            
+            var groupedPeople = isrpos.GroupBy(p => GetCategory(p.vozrast)).ToList();
+            var cat1 = isrpos.Where(p => GetCategory(p.vozrast) == 1);
+            var cat2 = isrpos.Where(p => GetCategory(p.vozrast) == 2);
+            var cat3 = isrpos.Where(p => GetCategory(p.vozrast) == 3);
+            var app = new Word.Application();
+            Word.Document document = app.Documents.Add();
+            using (sadEntities usersEntities = new sadEntities())
+            {
+                int i = 1;
+                foreach (var group in groupedPeople)
+                {
+                    int j = 1;
+                    Word.Paragraph paragraph = document.Paragraphs.Add();
+                    Word.Range range = paragraph.Range;
+                    range.Text = Convert.ToString(i);
+                    paragraph.set_Style("Заголовок 1");
+                    range.InsertParagraphAfter();
+                    Word.Paragraph tableParagraph =
+                    document.Paragraphs.Add();
+                    Word.Range tableRange = tableParagraph.Range;
+                    Word.Table studentsTable =
+                    document.Tables.Add(tableRange, group.Count() + 1, 3);
+                    studentsTable.Borders.InsideLineStyle =
+                    studentsTable.Borders.OutsideLineStyle =
+                    Word.WdLineStyle.wdLineStyleSingle;
+                    studentsTable.Range.Cells.VerticalAlignment =
+                    Word.WdCellVerticalAlignment.wdCellAlignVerticalCenter;
+                    Word.Range cellRange;
+                    cellRange = studentsTable.Cell(1, 1).Range;
+                    cellRange.Text = "ФИО";
+                    cellRange = studentsTable.Cell(1, 2).Range;
+                    cellRange.Text = "КОД";
+                    cellRange = studentsTable.Cell(1, 3).Range;
+                    cellRange.Text = "email";
+                    studentsTable.Rows[1].Range.Bold = 1;
+                    studentsTable.Rows[1].Range.ParagraphFormat.Alignment =
+                    Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    foreach (var s in isrpos)
+                    {
+                        if (GetCategory(s.vozrast) == i)
+                        {
+                            cellRange = studentsTable.Cell(j + 1, 1).Range;
+                            cellRange.Text = s.FIO.ToString();
+                            cellRange.ParagraphFormat.Alignment =
+                            Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                            cellRange = studentsTable.Cell(j + 1, 2).Range;
+                            cellRange.Text = s.KOD;
+                            cellRange.ParagraphFormat.Alignment =
+                            Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                            cellRange = studentsTable.Cell(j + 1, 3).Range;
+                            cellRange.Text = s.email;
+                            cellRange.ParagraphFormat.Alignment =
+                            Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                            j++;
+                        }
+                    }
+                    i++;
+                }
+            }
+            
+            app.Visible = true;
+        }
+
+        private void b4_Click(object sender, RoutedEventArgs e)
+        {
+            string json = File.ReadAllText("D:\\лабы\\Импорт\\3.json");
+            p = JsonConvert.DeserializeObject<person[]>(json);
+            using (sadEntities entities = new sadEntities())
+            {
+                entities.SaveChanges();
+                foreach (person p in p)
+                {
+                    if (p.FullName != null)
+                        entities.isrpo.Add(new isrpo(p.FullName, p.CodeClient, p.BirthDate, p.Index, p.City, p.Street, p.Home, p.Kvartira, p.E_mail));
+                }
+                entities.SaveChanges();
+
+            }
+
         }
     }
 }
